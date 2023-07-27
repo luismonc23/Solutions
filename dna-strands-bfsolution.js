8e74d4ee81224ab5d0b5aa163cae78a477c4effb
luismonc23 <35046829+luismonc23@users.noreply.github.com>
2023-07-27 16:32:57 -0600

    Update dna-strands-bfsolution.js
    added code objective

--- dna-strands-bfsolution.js
+++ dna-strands-bfsolution.js
@@ -1,3 +1,7 @@
+//In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". 
+//Your function receives one side of the DNA you need to return the other complementary side
+//  . DNA strand is never empty or there is no DNA at all.
+
 function DNAStrand(dna){
   debugger;
   let chars = dna.split('');
