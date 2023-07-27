8420a7e06623cf07b770d432737aa442bea545b5
luismonc23 <35046829+luismonc23@users.noreply.github.com>
2023-07-27 16:53:11 -0600

    Create regex-pin-validation-bfsolution.js

---
+++ regex-pin-validation-bfsolution.js
@@ -0,0 +1,11 @@
+//ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+//If the function is passed a valid PIN string, return true, else return false.
+  
+function validatePIN (pin) {
+  if(pin.length == 4)
+    return RegExp("(^[0-9]{4}$)").test(pin);
+  if(pin.length == 6)
+    return RegExp("(^[0-9]{6}$)").test(pin);
+  else return false;
+}
