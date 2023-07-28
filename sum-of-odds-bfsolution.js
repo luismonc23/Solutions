a235b5b84e54b45e6a4e13fc28ff8092a72d80cb
luismonc23 <35046829+luismonc23@users.noreply.github.com>
2023-07-28 12:27:20 -0600

    Update sum-of-odds-bfsolution.js
    Added more details

--- sum-of-odds-bfsolution.js
+++ sum-of-odds-bfsolution.js
@@ -6,12 +6,14 @@
 // Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
 // 1 -->  1
 // 2 --> 3 + 5 = 8
+
+//alternative way to elevating n to the power of 3:
   
 function rowSumOddNumbers(n) {
 	// TODO
-  let nthRowStart = 1 + (n*(n-1));
-  let nthRowNumberOfElements = n;
-  let totalSum = nthRowStart;
+  let nthRowStart = 1 + (n*(n-1)); //this represents the sequence of the first element of each row of the pyramid
+  let nthRowNumberOfElements = n; //the number of the level of the pyramid contains that same number of elements
+  let totalSum = nthRowStart; //The sum starts with the first element in the row, previously calculated
   
   for(i= 1; i<nthRowNumberOfElements; i++){
     nthRowStart+=2;
