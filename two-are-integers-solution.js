9df3fc213a240edf2bde91be64b0047e7b399b4c
luismonc23 <35046829+luismonc23@users.noreply.github.com>
2023-07-28 15:52:53 -0600

    Update two-are-integers-solution.js
    Updated solution to a more compact approach

--- two-are-integers-solution.js
+++ two-are-integers-solution.js
@@ -3,14 +3,8 @@
 // (greater than zero), and False - otherwise.
   
 function twoArePositive(a, b, c) {
-  let positiveInt = 0;
-    if(a > 0)
-      positiveInt++;
-    if(b > 0)
-      positiveInt++;
-    if(c > 0)
-      positiveInt++;
-    
-    if(positiveInt == 2) return true;
-    else return false;
+  let positiveInt = 0;  
+  positiveInt += (a > 0) + (b > 0) + (c > 0) //if evaluates to true, is also a 1, false is a 0
+  if(positiveInt == 2) return true;
+  else return false;
 }
