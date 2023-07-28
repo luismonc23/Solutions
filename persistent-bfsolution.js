afbd8fd5a379abb1651a4ae4e445f35b51041440
luismonc23 <35046829+luismonc23@users.noreply.github.com>
2023-07-28 16:49:10 -0600

    Update persistent-bfsolution.js
    Added default value in function parameter "count"

--- persistent-bfsolution.js
+++ persistent-bfsolution.js
@@ -1,11 +1,8 @@
 //Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the 
   //number of times you must multiply the digits in num until you reach a single digit.
 
-function persistence(num, count) {
-    
+function persistence(num, count=0) {
     //code me
-    if(typeof count == 'undefined')
-        count = 0;
     if(count == 0 && num < 10) return count;
     count++;
   
