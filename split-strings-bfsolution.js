872b827504e9a9b13d0a85b54ac9b4c29bbe424b
luismonc23 <35046829+luismonc23@users.noreply.github.com>
2023-08-08 17:04:30 -0600

    Update split-strings-bfsolution.js
    Added description

--- split-strings-bfsolution.js
+++ split-strings-bfsolution.js
@@ -2,18 +2,18 @@
 // If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
 
 function solution(str){
-    let chars = str.split('');
-    let count = 0;
+    let chars = str.split(''); //I could add an _ to the end of the initial string, if its odd, it will complete, if not, the underscore will be ignored
+    let count = 0; //How many characters have been added
     let tempString = "";
     let collectedElements = [];
     let isOdd = str.length % 2 == 1;
 
-   for (let i = 0; i < (isOdd ? chars.length + 1 : chars.length); i++) {
+   for (let i = 0; i < (isOdd ? chars.length + 1 : chars.length); i++) { //If its odd, it runs until the length + 1
         if(count < 1){
             tempString += chars[i];
             count++;
         } else {
-            tempString += chars[i] ?? "_";
+            tempString += chars[i] ?? "_"; //If the character is empty because its an odd length string, add the underscore
             count++;
             collectedElements.push(tempString);
             count = 0;
