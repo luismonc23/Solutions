a3564419e7077dfce0e73044603f2ec9660b78cd
luismonc23 <35046829+luismonc23@users.noreply.github.com>
2023-08-12 18:11:57 -0600

    Update roman-numerals-bfsolution.js
    Added description to the solution

--- roman-numerals-bfsolution.js
+++ roman-numerals-bfsolution.js
@@ -11,27 +11,29 @@ function solution(number){
   let u = "";
   // convert the number to a roman numeral
   if(number > 999){
-    m = Math.floor(number / 1000);
+    m = Math.floor(number / 1000); //get the thousands
     number -= m * 1000;
   }
   else m = "";
   if(number > 99){
-    c = Math.floor(number / 100);
+    c = Math.floor(number / 100); //get the hundreds
     number -= c * 100;
   }
   else c = "";
   if(number > 9){
-    d = Math.floor(number / 10);
+    d = Math.floor(number / 10); //get the tens
     number -= d * 10;
   }
   else d = "";
   if(number < 10)
-    u = number;
+    u = number;  //get the units
 
   let numArray = [m,c,d,u];
   finalString = "M".repeat(m) + 
     (c < 4 ? "C".repeat(c) : c == 4 ? "CD" : c == 9 ? "CM" : "D" + "C".repeat(c-5)) + 
     (d < 4 ? "X".repeat(d) : d == 4 ? "XL" : d == 9 ? "XC" : "L" + "X".repeat(d-5)) + 
     (u < 4 ? "I".repeat(u) : u == 4 ? "IV" : u == 9 ? "IX" : "V" + "I".repeat(u-5));
+  //M repeats max 3 times because number must be less than 3999
+  //C repeats up to three times, or is a special case (4,9), or is greater than 5, same with tens (X) and units (I) 
   return finalString;
 }
