51e5826686a1550f7ac09d86e73045794bfbef33
luismonc23 <35046829+luismonc23@users.noreply.github.com>
2023-08-17 17:12:59 -0600

    Update create-phone-number-altsolution.js

--- create-phone-number-altsolution.js
+++ create-phone-number-altsolution.js
@@ -3,6 +3,6 @@
 //returns "(123) 456-7890"
 
 function createPhoneNumber(numbers){
-  let a = numbers.map(String);
+  let a = numbers.map(String); //This function maps each number to its value as string
   return `(${a[0]+a[1]+a[2]}) ${a[3]+a[4]+a[5]}-${a[6]+a[7]+a[8]+a[9]}`; //Template literals
 }
